2bf5ee84b78b08e5316e89c2d8cbe03861a6ec42
Petras-aleksandar988 <104795401+Petras-aleksandar988@users.noreply.github.com>
2023-04-22 11:43:06 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,40 @@
+const tabLinks = document.querySelectorAll(".tab-links");
+const tabContents = document.querySelectorAll(".tab-contents");
+
+function clickTab(tabname) {
+  current = event.currentTarget;
+  for (let tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+  for (let tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  current.classList.add("active-link");
+  document.querySelector(`#${tabname}`).classList.add("active-tab");
+}
+
+const listUl = document.querySelector("#listUl");
+console.log(listUl);
+function openMenu() {
+  listUl.style.right = 0;
+}
+function closeMenu() {
+  listUl.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwoumaDlKxNJsx-88XJG5dnvxz7Z3UvZtGH_4yXkX0nGfuoKAXC2BOWIFVUPXJgzLdc/exec";
+const form = document.forms["submit-to-google-sheet"];
+const message = document.querySelector("#message");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+        message.innerHTML = "Message sent successfully!";
+        setTimeout(() => {
+            message.innerHTML = ""
+        }, 5000)
+        form.reset()
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
